038b8e7836023ebf47152be886ec4de594daa42e
Saleh Alkhalifa <salehesam@gmail.com>
2020-12-26 18:33:03 -0500

    Finished controller

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,14 @@
-const snippetService = require('../services/snippet.service.server')
+const bookService = require('../services/book.service.server')
 
 module.exports = function (app) {
 
-    app.get('/api/snippets/', (req, res) =>
-        snippetService.findAllSnippets()
-            .then(allSnippets => res.json(allSnippets)));
+    app.get('/api/books/', (req, res) =>
+        bookService.findAllBooks()
+            .then(allBooks => res.json(allBooks)));
+
+    app.get('/api/books/:bid', (req, res) =>
+        bookService.findBooksById(req.params['bid'])
+            .then(book => res.json(book)));
 
     // const documentation = (req, res) => {
     //     //     res.sendFile('documentation.html', {root: __dirname })
